622dbf726ec676b3f8a07905f863d23cf690c6fb
Shikiyo <shikiyo@archjk>
2022-09-12 10:33:24 -0300

    Properly making https request

--- src/main/java/com/kiyoshi364/Main.java
+++ src/main/java/com/kiyoshi364/Main.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.StringBuilder;
 import java.net.URL;
 import java.net.MalformedURLException;
 import javax.net.ssl.HttpsURLConnection;
@@ -22,10 +23,10 @@ public class Main {
         System.out.println();
 
         GitSearchRequest search = new GitSearchRequest("crlf asdf");
+        String link = search.makeLink();
         System.out.println("Search:");
-        System.out.println(search.makeLink());
-        // Web.make_connection(
-        //         search, Web.Method.GET );
+        System.out.println(link);
+        Web.makeRequest(link, Web.Method.GET, null);
     }
 }
 
@@ -35,18 +36,24 @@ final class Web {
         GET, POST, HEAD, OPTIONS, PUT, DELETE, TRACE;
     }
 
-    public static void make_connection(
-            GitSearchRequest search, Method method) {
-        String link = search.makeLink();
-        System.out.printf("URL: %s\n", link);
+    public static String makeRequest(
+            String link, Method method, String token) {
         try {
             URL url = new URL(link);
             HttpsURLConnection conn
                 = (HttpsURLConnection) url.openConnection();
-            // conn.setRequestMethod(method.toString());
-            // conn.setDoOutput(true);
+            conn.setRequestMethod(method.toString());
+            conn.setRequestProperty(
+                    "Accept", "application/vnd.github+json");
+            if ( token != null ) {
+                conn.setRequestProperty(
+                        "Authorization", "Bearer " + token);
+            }
+            conn.setConnectTimeout(5000);
+            conn.setReadTimeout(5000);
+            conn.setInstanceFollowRedirects(false);
 
-            // conn.connect();
+            conn.connect();
             int rcode = conn.getResponseCode();
             String rmsg = conn.getResponseMessage();
             System.out.printf("Response: %d %s\n",
@@ -60,11 +67,17 @@ final class Web {
             }
             BufferedReader buffer
                 = new BufferedReader(new InputStreamReader(in));
-            String inputLine;
-            while ( (inputLine = buffer.readLine()) != null ) {
-                System.out.println(inputLine);
+            StringBuilder b = new StringBuilder();
+            char[] buff = new char[1024];
+            int read = 0;
+            while ( (read = buffer.read(buff,0,buff.length)) != -1 ) {
+                System.out.print(buff);
+                b.append(buff);
             }
+            System.out.println();
             buffer.close();
+            conn.disconnect();
+            return b.toString();
         } catch (MalformedURLException e) {
             System.out.println(e);
             System.exit(1);
@@ -72,5 +85,6 @@ final class Web {
             System.out.println(e);
             System.exit(1);
         }
+        return null;
     }
 }
