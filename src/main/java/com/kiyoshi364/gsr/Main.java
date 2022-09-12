b0e8110c00a4a1a2c59bcc0ea0519a6d69addbb7
Shikiyo <shikiyo@archjk>
2022-09-12 16:29:20 -0300

    Basic server functionality

--- src/main/java/com/kiyoshi364/gsr/Main.java
+++ src/main/java/com/kiyoshi364/gsr/Main.java
@@ -1,43 +1,47 @@
 package com.kiyoshi364.gsr;
 
 import com.google.gson.Gson;
+import java.io.IOException;
 
 public final class Main {
 
-    public static void help_message() {
-        System.out.printf("usage: <subcommand>\n");
-    }
-
     public static void main(String[] args) {
-        System.out.print("Hello World:");
-        for ( String arg : args ) {
-            System.out.print(" " + arg);
-        }
-        System.out.println();
 
-        GitSearchRequest search = new GitSearchRequest("crlf");
-        String link = search.makeLink();
-        System.out.print("Search: ");
-        System.out.println(link);
-        Web.Response resp
-            = Web.makeRequest(link, Web.Method.GET, null);
-            // = Web.fakeRequest(link, Web.Method.GET, null);
-        Gson gson = new Gson();
-        switch ( resp.rcode ) {
-            case 200:
-                GitSearchResponse gsr
-                    = gson.fromJson(
-                        resp.response, GitSearchResponse.class);
-                System.out.println("Response:");
-                System.out.println(gsr.toString());
-                break;
-            case 304:
-            case 422:
-            case 503:
-                System.out.printf("Response: %d %s\n",
-                    resp.rcode, resp.rmsg);
-                System.out.println(resp.response);
-                break;
+        Server.Config config = new Server.Config(args);
+        System.out.print("PORT: ");
+        System.out.println(config.port);
+
+        Server server = new Server(config);
+        try {
+            server.run();
+        } catch (IOException e) {
+            System.out.println(e);
+            System.exit(1);
         }
+
+        // GitSearchRequest search = new GitSearchRequest("crlf");
+        // String link = search.makeLink();
+        // System.out.print("Search: ");
+        // System.out.println(link);
+        // Web.Response resp
+        //     // = Web.makeRequest(link, Web.Method.GET, null);
+        //     = Web.fakeRequest(link, Web.Method.GET, null);
+        // Gson gson = new Gson();
+        // switch ( resp.rcode ) {
+        //     case 200:
+        //         GitSearchResponse gsr
+        //             = gson.fromJson(
+        //                 resp.response, GitSearchResponse.class);
+        //         System.out.println("Response:");
+        //         System.out.println(gsr.toString());
+        //         break;
+        //     case 304:
+        //     case 422:
+        //     case 503:
+        //         System.out.printf("Response: %d %s\n",
+        //             resp.rcode, resp.rmsg);
+        //         System.out.println(resp.response);
+        //         break;
+        // }
     }
 }
---
+++ src/main/java/com/kiyoshi364/gsr/Server.java
@@ -0,0 +1,93 @@
+package com.kiyoshi364.gsr;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.lang.NumberFormatException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public final class Server {
+    private final Config config;
+
+    public static final class Config {
+        public static final int DEFAULT_PORT = 8000;
+
+        public final int port;
+
+        public Config(String[] args) {
+            // TODO: use args for something
+            String port_str = System.getenv("PORT");
+            int port = DEFAULT_PORT;
+            if ( port_str != null ) {
+                try {
+                    port = Integer.parseUnsignedInt(port_str);
+                } catch (NumberFormatException e) {
+                    System.out.println("WARNING:"
+                            + " unable to parse PORT"
+                            + " environment variable: `"
+                            + port_str
+                            + "'");
+                    port = DEFAULT_PORT;
+                }
+            }
+            this.port = port;
+        }
+    }
+
+    public Server(Config config) {
+        this.config = config;
+    }
+
+    public Server(String[] args) {
+        this(new Config(args));
+    }
+
+    public static String readInput(InputStream in) throws IOException {
+        BufferedReader buffer = new BufferedReader(
+                new InputStreamReader(in));
+        StringBuilder b = new StringBuilder();
+        String line = null;
+        while ( (line = buffer.readLine()).length() > 0 ) {
+            b.append(line);
+            b.append("\n");
+        }
+        return b.toString();
+    }
+
+    public void run() throws IOException {
+        System.out.println("Running . . .");
+
+        ServerSocket server = null;
+        try {
+            server = new ServerSocket(this.config.port);
+        } catch (IOException e) {
+            throw new IOException("Could not create Server", e);
+        }
+
+        try {
+            while ( true ) {
+                Socket socket = server.accept();
+                System.out.println(">>>> New client <<<<");
+
+                // TODO: Note: maybe use threads?
+                String request = readInput(socket.getInputStream());
+                System.out.println(request);
+
+                PrintWriter out =
+                    new PrintWriter(socket.getOutputStream());
+                out.println(request);
+                out.flush();
+
+                socket.close();
+            }
+        } catch (IOException e) {
+            throw new IOException("Could not handle connection", e);
+        } finally {
+            server.close();
+        }
+    }
+}
